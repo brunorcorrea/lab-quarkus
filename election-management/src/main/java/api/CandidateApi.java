38675bb5c1ebd964949dadd0b6adff49cb6f1b8a
Bruno Correa <correaricardobruno@gmail.com>
2025-01-11 14:25:27 -0300

    create api layer

---
+++ election-management/src/main/java/api/CandidateApi.java
@@ -0,0 +1,33 @@
+package api;
+
+import api.dto.in.CreateCandidate;
+import api.dto.in.UpdateCandidate;
+import api.dto.out.Candidate;
+import domain.CandidateService;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class CandidateApi {
+
+    private final CandidateService candidateService;
+
+    public CandidateApi(CandidateService candidateService) {
+        this.candidateService = candidateService;
+    }
+
+    public void create(CreateCandidate dto) {
+        candidateService.save(dto.toDomain());
+    }
+
+    public api.dto.out.Candidate update(String id, UpdateCandidate dto) {
+        candidateService.save(dto.toDomain(id));
+
+        return api.dto.out.Candidate.fromDomain(candidateService.findById(id));
+    }
+
+    public List<api.dto.out.Candidate> list() {
+        return candidateService.findAll().stream().map(Candidate::fromDomain).toList();
+    }
+}
