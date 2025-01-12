81f148b883b4eca460058d6f1dfa6d73bacd86ce
Bruno Correa <correaricardobruno@gmail.com>
2025-01-12 13:52:57 -0300

    add redis repository

--- election-management/src/main/java/infraestructure/repositories/RedisElectionRepository.java
+++ election-management/src/main/java/infraestructure/repositories/RedisElectionRepository.java
@@ -2,21 +2,34 @@ package infraestructure.repositories;
 
 import domain.Election;
 import domain.ElectionRepository;
+import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.pubsub.PubSubCommands;
+import io.quarkus.redis.datasource.sortedset.SortedSetCommands;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class RedisElectionRepository implements ElectionRepository {
 
-    private final EntityManager entityManager;
+    private final SortedSetCommands<String, String> commands;
+    private final PubSubCommands<String> pubSubCommands;
 
-    public RedisElectionRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
+    public RedisElectionRepository(RedisDataSource redisDataSource) {
+        commands = redisDataSource.sortedSet(String.class, String.class);
+        pubSubCommands = redisDataSource.pubsub(String.class);
     }
 
     @Override
     @Transactional
     public void submit(Election election) {
+        Map<String, Double> rank = election.votes().entrySet().stream()
+                .collect(Collectors.toMap(entry -> entry.getKey().id(),
+                        entry -> entry.getValue().doubleValue()));
+
+        commands.zadd("election:" + election.id(), rank);
+        pubSubCommands.publish("elections", election.id());
     }
 }
